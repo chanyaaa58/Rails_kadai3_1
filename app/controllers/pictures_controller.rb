c772aa7b578c5751a82cc2dd30bb164e0fb56c5a
chanyaaa58 <kanegonaja@gmail.com>
2021-08-20 18:11:35 +0900

    Controllerの共通処理のメソッド化、Viewのパーシャル化

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,5 @@
 class PicturesController < ApplicationController
+  before_action :set_picture, only: [:show, :edit, :update]
   def index
     @pictures = Picture.all
   end
@@ -14,13 +15,10 @@ class PicturesController < ApplicationController
     end
   end
   def show
-    @picture = Picture.find(params[:id])
   end
   def edit
-    @picture = Picture.find(params[:id])
   end
   def update
-    @picture = Picture.find(params[:id])
     if @picture.update(picture_params)
       redirect_to pictures_path, notice: "投稿を編集しました！"
     else
@@ -30,5 +28,8 @@ class PicturesController < ApplicationController
   private
   def picture_params
     params.require(:picture).permit(:title, :content)
+  end
+  def set_picture
+    @picture = Picture.find(params[:id])
   end
 end
