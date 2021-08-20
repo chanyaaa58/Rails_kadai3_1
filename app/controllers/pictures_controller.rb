5fc5dd9785a03eac75f66e7f19d420dd75f60009
chanyaaa58 <kanegonaja@gmail.com>
2021-08-20 17:59:47 +0900

    edit・updateアクションを定義

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -16,6 +16,17 @@ class PicturesController < ApplicationController
   def show
     @picture = Picture.find(params[:id])
   end
+  def edit
+    @picture = Picture.find(params[:id])
+  end
+  def update
+    @picture = Picture.find(params[:id])
+    if @picture.update(picture_params)
+      redirect_to pictures_path, notice: "投稿を編集しました！"
+    else
+      render :edit
+    end
+  end
   private
   def picture_params
     params.require(:picture).permit(:title, :content)
