2c8c17f26d268f649b395d14d52418b61d22e628
chanyaaa58 <kanegonaja@gmail.com>
2021-08-26 11:10:40 +0900

    CSS実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'home/top'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
   resources :pictures do
