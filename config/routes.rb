c64a944ca85b989bbde4b0c18be30d8f7b1ec645
chanyaaa58 <kanegonaja@gmail.com>
2021-08-26 13:39:05 +0900

    init

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   get 'home/top'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
+  root'pictures#index'
   resources :pictures do
     collection do
       post :confirm
     end
-  end  
+  end
 end
